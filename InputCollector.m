166d8a56d005c8c9fae52604810cf8a932fe73c7
olga-nest <olga.w.nesterova@gmail.com>
2017-10-03 21:05:01 -0700

    Add input collector

--- InputCollector.m
+++ InputCollector.m
@@ -10,4 +10,23 @@
 
 @implementation InputCollector
 
+-(NSString *)inputForPrompt:(NSString *)promptString {
+    NSLog(@"What would you like do next? \n - Create a new contact list \n - List all contacts quit \n - Exit Application >> \n");
+    
+    char answerCString;
+    
+    fgets(&answerCString, 255, stdin);
+    
+    NSString *result = [NSString stringWithCString:&answerCString
+                                          encoding:NSUTF8StringEncoding];
+    
+    //removes new line and white spaces
+ //   NSCharacterSet *resultSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSString *parsedResult = [result stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    return parsedResult;
+
+    
+}
+
 @end
--- main.m
+++ main.m
@@ -1,8 +1,30 @@
 #import <Foundation/Foundation.h>
+#import "InputCollector.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-    }
-    return 0;
+        BOOL appIsRunning = YES;
+        
+        InputCollector *inputCollector = [[InputCollector alloc]init];
+        
+        while (appIsRunning == YES) {
+        
+        
+        NSString *parsedResult = [inputCollector inputForPrompt:@"..."];
+
+            
+            if ([parsedResult isEqual: @"quit"]) {
+                NSLog(@"See you!");
+                appIsRunning = NO;
+                continue;
+            } else {
+                
+       }
+    
+        
+        }
+        return 0;
+    
+}
 }
