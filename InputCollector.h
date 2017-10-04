b2170026cd7b849ac72fcee70e5b209a9c2bfb25
olga-nest <olga.w.nesterova@gmail.com>
2017-10-03 18:17:33 -0700

    Initizl commit

---
+++ InputCollector.h
@@ -0,0 +1,13 @@
+//
+//  InputCollector.h
+//  ContactList
+//
+//  Created by Olga on 10/3/17.
+//  Copyright © 2017 Olga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InputCollector : NSObject
+
+@end
---
+++ InputCollector.m
@@ -0,0 +1,13 @@
+//
+//  InputCollector.m
+//  ContactList
+//
+//  Created by Olga on 10/3/17.
+//  Copyright © 2017 Olga. All rights reserved.
+//
+
+#import "InputCollector.h"
+
+@implementation InputCollector
+
+@end
